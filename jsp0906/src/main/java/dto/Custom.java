f08eaf4012792f2786ead01475d34ab440b0f59d
andylee-dev <andylee.devknight@gmail.com>
2023-09-08 17:15:49 +0900

    [draft] add Custom DTO

---
+++ jsp0906/src/main/java/dto/Custom.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class Custom {
+	private int sabun;
+	private String name;
+	private int sal;
+	private String handphone;
+	public int getSabun() {
+		return sabun;
+	}
+	public void setSabun(int sabun) {
+		this.sabun = sabun;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public String getHandphone() {
+		return handphone;
+	}
+	public void setHandphone(String handphone) {
+		this.handphone = handphone;
+	}
+}
