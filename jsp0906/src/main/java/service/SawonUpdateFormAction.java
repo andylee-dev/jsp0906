4c779a6a8a679e603d446a83dc81ed17b401a340
andylee-dev <andylee.devknight@gmail.com>
2023-09-12 17:15:40 +0900

    [feat] add sawon table update feature

---
+++ jsp0906/src/main/java/service/SawonUpdateFormAction.java
@@ -0,0 +1,31 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+import dao.SawonDao;
+import dto.Sawon;
+
+
+public class SawonUpdateFormAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws SerialException, IOException {
+		System.out.println(getClass().getName()+"-requestPro Start");
+		try {
+			int sabun = Integer.parseInt(request.getParameter("sabun"));
+			SawonDao sd = SawonDao.getInstance();
+			Sawon sawon =sd.select(sabun);
+			request.setAttribute("sawon", sawon);
+		
+		} catch (Exception e) {
+			System.out.println("[ERROR]"+getClass().getName()+":"+e.getMessage());
+		}
+		return "sawonUpdateForm.jsp";
+	}
+
+}
---
+++ jsp0906/src/main/java/service/SawonUpdateProAction.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+import dao.SawonDao;
+import dto.Sawon;
+
+public class SawonUpdateProAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws SerialException, IOException {
+		System.out.println(getClass().getName()+"-requestPro Start");
+		try {
+			request.setCharacterEncoding("utf-8");
+			
+			Sawon sawon = new Sawon();
+			sawon.setSabun(Integer.parseInt(request.getParameter("sabun")));
+			sawon.setName(request.getParameter("name"));
+			sawon.setSal(Integer.parseInt(request.getParameter("sal")));		
+			sawon.setHandphone(request.getParameter("handphone"));
+			
+			SawonDao sd = SawonDao.getInstance();
+			int result = sd.update(sawon);
+			request.setAttribute("sabun", sawon.getSabun());
+			request.setAttribute("result", result);
+		} catch (Exception e) {
+			System.out.println("[ERROR]"+getClass().getName()+":"+e.getMessage());
+		}
+		return "sawonUpdatePro.jsp";
+	}
+
+}
