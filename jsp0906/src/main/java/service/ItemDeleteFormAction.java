c644f3ecb7fbcf0d56f417b48d95aea384cfc4e4
andylee-dev <andylee.devknight@gmail.com>
2023-09-13 16:07:11 +0900

    [feat] add Item Delete feature

---
+++ jsp0906/src/main/java/service/ItemDeleteFormAction.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+import dao.ItemDao;
+import dto.Item;
+
+public class ItemDeleteFormAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws SerialException, IOException {
+		System.out.println(getClass().getName()+"-requestPro Start");
+		try {
+			int item_code = Integer.parseInt(request.getParameter("item_code"));
+			ItemDao itemDao = ItemDao.getInstance();
+			Item item =itemDao.select(item_code);
+			request.setAttribute("item", item);
+		
+		} catch (Exception e) {
+			System.out.println("[ERROR]"+getClass().getName()+":"+e.getMessage());
+		}
+		return "itemDeleteForm.jsp";
+	}
+
+}
---
+++ jsp0906/src/main/java/service/ItemDeleteProAction.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+import dao.ItemDao;
+
+public class ItemDeleteProAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws SerialException, IOException {
+		System.out.println(getClass().getName()+"-requestPro Start");
+		try {
+			int item_code = Integer.parseInt(request.getParameter("item_code"));
+
+			ItemDao itemDao = ItemDao.getInstance();
+			int result = itemDao.delete(item_code);
+			request.setAttribute("result", result);
+		
+		} catch (Exception e) {
+			System.out.println("[ERROR]"+getClass().getName()+":"+e.getMessage());
+		}
+		return "itemDeletePro.jsp";
+	}
+
+}
